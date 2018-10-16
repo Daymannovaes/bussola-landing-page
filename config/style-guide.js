a15efb7a37a0d0dde78385b48de79df1eb4aab58
Dayman Novaes <daymannovaes@gmail.com>
2018-10-16 11:05:57 -0300

    add babelrc and add eslint rules

--- config/style-guide.js
+++ config/style-guide.js
@@ -1,5 +1,6 @@
 export const PADDING_UNIT = 6;
 export const BORDER_RADIUS = 6;
+export const POST_WIDTH = 740;
 export const LOGO_HEIGHT = 8; // em unit
 
 export const colors = {
@@ -8,5 +9,10 @@ export const colors = {
     deepblue: '#055170',
     midblue: '#09688f',
     lightblue: '#61828d',
-    lightblack: '#34464c'
+    lightblack: '#34464c',
+
+    transparent: {
+        light: '#00000005'
+    }
 };
+
