608949a2adf766b7c56500e34266a4e344e94275
Dayman Novaes <daymannovaes@gmail.com>
2018-10-16 20:59:42 -0300

    text #1 almost done

---
+++ pages/como-ter-motivacao-e-fazer-o-que-nos-propomos.js
@@ -0,0 +1,196 @@
+import React from 'react';
+
+import LayoutPost from '../components/LayoutPost';
+import PostImg from '../components/PostImg';
+import Tip from '../components/Tip';
+import Diagrama from '../components/texto-1-diagrama';
+
+export default () => (
+    <LayoutPost className="text-container">
+        <h2>Como ter motivação na faculdade e sempre fazer o que nos propomos a fazer?</h2>
+
+        <h3>Porque não temos motivação?</h3>
+
+        <p>Existem muitas tarefas que nos motivam e nos dão muito prazer quando as fazemos, e quando pensamos nessas tarefas, dificilmente pensamos em <strong>faculdade</strong>.</p>
+
+        <p>Esse desapontamento já é bem comum mesmo nos períodos inicias da faculdade, e se esse é o seu caso, acredito que você tenha dito a você mesmo que “nas matérias mais pra frente do curso deve ficar melhor”. Mas será que as pessoas que estão há mais tempo no curso compartilham dessa opinião?</p>
+
+        <p>A faculdade é uma daquelas coisas que de alguma forma sentimos que precisamos fazer quando olhamos a longo prazo, mas que mesmo assim não nos sentimos felizes ao fazer no curto prazo, no dia a dia.</p>
+
+        <Diagrama src="/static/texto-1-diagrama-tarefas-1.svg" />
+
+        <p>O dilema representado pelo diagrama acima nos acompanhou a vida toda, desde a nossa infância temos que fazer coisas que não concordamos. E se antes éramos obrigados por força de nossos pais, atualmente somos obrigados por uma força invisível a qual chamamos de sociedade.</p>
+
+        <Tip>Existe alguma tarefa que você sente que precisa fazer, mas não quer fazer? Talvez no seu trabalho, ou com seus amigos, ou em casa? Pense em uma, anote em um papel e no final do texto conversaremos sobre isso.</Tip>
+
+        <p>Sem motivação, recorremos à qualquer justificativa de que lá na frente isso deve valer a pena. Geralmente o diploma, ou então as “possibilidades de trabalho”, sem ter exatamente certeza do que isso quer dizer.</p>
+
+        <p>Mas como então, resolver esse problema? Como encontrar motivação no dia a dia da faculdade?</p>
+
+        <p>Eu não tenho a resposta.</p>
+
+        <p>Brincadeira, tenho sim, se não, não estaria escrevendo esse texto. Mas preciso da sua ajuda para construir a resposta, pois ela não é simples, e ela não surgirá de uma hora para outra, como num passe de mágica.</p>
+
+        <h3>Como encontrar motivação?</h3>
+
+        <p>Para o nosso contexto aqui, existem <strong>duas</strong> coisas que influenciam o quão bem nos sentimos no presente ao executar alguma tarefa. A <strong>primeira</strong> é a sensação de que determinada tarefa nos leva em direção à um objetivo o qual queremos atingir.</p>
+
+        <Tip>Faça a pergunta a si mesmo: essa tarefa que estou fazendo, esse estudo, contribui em algo para o que eu espero do futuro?</Tip>
+
+        <p>Meio complexo e difícil refletir sobre isso né? A questão é que quando não temos certeza de onde queremos chegar, é difícil enxergar se as tarefas do dia a dia nos levam ou não para esse lugar.</p>
+
+        <p>Geralmente acreditamos no que as pessoas mais próximas nos dizem, sem ter certeza se existe outra escolha melhor, justamente porque não sabermos definir direito o que é esse “melhor”. Devo seguir na área acadêmica ou ir para o mercado de trabalho? Em qual área de profissão me especializar? Devo me especializar em alguma área? Qual é melhor?</p>
+
+        <p>A <strong>segunda</strong> coisa é a sensação de progresso. Sabe aquela sensação boa de riscar uma tarefa da sua lista, de ter conquistado algo? Aquela sensação de ter as obrigações cumpridas e poder descansar a cabeça tranquila no travesseiro à noite?</p>
+
+        <p>Mais fácil essa né? Bom, talvez não tão fácil, principalmente a parte de “descansar tranquilo”, mas com certeza essa sensação de <strong>conquista</strong> acontece mais facilmente do que a de "estou cumprindo meu <strong>propósito"</strong>.</p>
+
+        <p>Em um mundo ideal, gostaríamos de que tudo pudesse nos dar essas duas sensações. Porém elas acontecem pouco. Consigo pensar em alguns prováveis momentos em que você experimentou ambas: quando viu seu nome na lista de aprovados da faculdade, quando foi aceito naquele emprego ou estágio que queria ou quando recebeu a nota final de uma matéria e viu que foi aprovado.</p>
+
+        <p>Nesses três exemplos, é possível que a sensação de propósito não esteja tão presente, ou não seja tão perceptível. Mas a questão é que essas conquistas não são quaisquer conquistas. São conquistas que fazem parte de um caminho que você projetou para a sua vida, e concluí-las traz esse <strong>significado</strong> a mais.</p>
+
+        <p>Esse tipo de conquista é diferente de concluir uma tarefa como: terminar de lavar a louça, aprender uma nova música no violão ou ter batido seu recorde de distância caminhada. Percebe que as duas sensações são independentes, porém não são excludentes?</p>
+
+        <p>Então, se modificarmos um pouco o nosso diagrama e incluirmos esse novo entendimento, ele fica assim:</p>
+
+        <Diagrama src="/static/texto-1-diagrama-tarefas-2.svg" />
+
+        <p>Ok, mas e agora? Bom, agora que temos uma clareza maior do que compõe a nossa motivação - progresso e propósito - vamos tentar algumas intervenções para chegar ao nosso mundo ideal, onde os três círculos se sobrepõem totalmente.</p>
+
+        <h3>Como construir uma realidade na qual gostaríamos de viver</h3>
+
+        <Diagrama src="/static/texto-1-diagrama-tarefas-3.svg" />
+
+        <p>Primeiro, acho que vale a pena dizer que é extremamente difícil definir o <strong>porque</strong> devemos fazer as nossas obrigações. Bom, pelo menos em um nível de profundidade que seja suficiente para nos motivar quando pensamos nisso.</p>
+
+        <Tip>Pegue a tarefa que você anotou no papel e pense: qual a primeira consequência ruim que irá acontecer se você simplesmente deixá-la para lá?</Tip>
+
+        <p>Obviamente, não queremos ter que lidar com as consequências negativas de não cumprir as nossas obrigações, e talvez essa possa ser a primeira coisa que pensamos ao refletir do <strong>porque</strong> fazê-la. Mas não vamos nos contentar com essa resposta, ok? Essa é uma razão muita externa, e queremos achar algo mais alinhado com o que acreditamos.</p>
+
+        <p>Antes de continuar refletindo sobre o porque das coisas, vamos falar um pouco sobre como podemos incluir mais progresso e mais conquistas no nosso dia a dia.</p>
+
+        <h4>Como progredir mais nas minhas tarefas do dia a dia?</h4>
+
+        <p>Nós já conseguimos decompor a nossa motivação em dois componentes: propósito e progresso.</p>
+
+        <p>E agora, para nós aprofundar nosso entendimento em <strong>progresso </strong>vamos decompor novamente em mais duas coisas.</p>
+
+        <p>A <strong>primeira</strong> coisa que precisamos entender com clareza é, justamente, a noção de <strong>decomposição</strong>.</p>
+
+        <p>Decompor é simplesmente dividir uma tarefa já existente, em tarefas menores. É algo que todos já fazemos em vários momentos, mas as vezes fazemos sem perceber, e entender como funciona para aplicarmos sempre que quisermos é extremamente útil.</p>
+
+        <p>Pegue o exemplo da lavagem de louças. É algo relativamente simples, que depois de um lanche da tarde no meio da semana conseguimos fazer em menos de 10 minutos. Mas depois de um almoço de domingo em família pode levar quase uma hora e ser muito chato.</p>
+
+        <p>Mas por mais chato e maçante que seja, lavar louças é simplesmente um conjunto de duas tarefas menores: ensaboar e enxaguar. Se na nossa <strong>lista de tarefas</strong>, ao invés de colocarmos “lavar louças”, colocarmos duas tarefas “ensaboar louças” e “enxaguar louças”, teremos o dobro da sensação de progresso.</p>
+
+        <PostImg src="/static/texto-1-lista-de-tarefas.jpg" />
+
+        <p>E nós podemos continuar decompondo cada tarefa menor em tarefas ainda menores. Pegando agora uma tarefa um pouco mais complexa, como por exemplo “dar uma limpeza geral na casa”.</p>
+
+        <p>É uma tarefa que pode demorar quase o dia todo, e que quando pensamos dessa forma, quando formulamos a tarefa dessa forma na nossa cabeça, podemos ter preguiça de fazê-la, justamente por ser uma tarefa grande, na qual a sensação de progresso está longe.</p>
+
+        <p>Mas quebrando as tarefas em tarefas cada vez menores, como</p>
+
+        <p><strong>Limpar a casa:</strong></p>
+
+        <ol>
+            <li>Limpar a cozinha</li>
+            <li>Limpar a sala</li>
+            <li>Limpar o quarto</li>
+            <li>etc...</li>
+        </ol>
+
+        <p>E fazendo isso novamente com cada tarefa:</p>
+
+        <p><strong>1. Limpar a cozinha:</strong></p>
+        <ul>
+            <li>1.1. Lavar as louças</li>
+            <li>1.2. Limpar o fogão</li>
+            <li>1.3. Organizar os armários</li>
+            <li>1.4. etc...</li>
+        </ul>
+
+        <p>E se continuarmos esse processo, até chegarmos em <strong>micro-tarefas</strong> que são simples e rápidas, podemos continuar avançando e progredindo rapidamente, com mais motivação. Melhor ainda se as tarefas contribuírem para algo que acreditarmos que precisamos.</p>
+
+        <p>Sabe aquele livro enorme e chato que você tem que ler para uma matéria da faculdade? Não dá preguiça? Mas você já calculou quantas páginas precisa ler por dia? Você pode chegar à conclusão de que precisa ler apenas 5 páginas por dia.</p>
+
+        <p>Dessa forma, “ler o livro grande e chato” não passa ser mais necessidade do dia a dia, mas sim “ler cinco páginas". Mais fácil, né?</p>
+
+        <h4>Mas porque isso é tão importante?</h4>
+
+        <p>Quando estamos sentados no sofá, confortáveis e descansando logo após ter chegado cansado em casa, e nos lembramos de nossas responsabilidades, a primeira coisa que nós pensamos, geralmente é a tarefa que definimos para nós mesmos que devemos cumprir.</p>
+
+        <PostImg src="/static/texto-1-pensativo-sofa-1.jpg" />
+
+        <p>E é importante que o próximo passo o qual nos propusemos de realizar, seja simples e fácil de executar. Dessa forma não precisamos tentar criar motivação artificialmente para tentar sair da inércia.</p>
+
+        <PostImg src="/static/texto-1-grafico-motivacao.jpg" />
+
+        <p>Quanto mais difícil a tarefa, mais motivação e força de vontade precisamos para realizá-la. Podemos tentar aumentar nossa força de vontade, ou tentar simplificar a tarefa, qual é mais fácil?</p>
+
+        <h4>Aplicando o método das micro-tarefas em outros contextos da vida</h4>
+
+        <p>Pensar em como podemos desconstruir nossas tarefas e simplificar a nossa vida é realmente muito útil, e podemos fazer isso com literalmente qualquer área de nossas vidas. Porém, depois que criamos esse hábito, justamente por trazer muitos benefícios a curto prazo, pode ser perigoso. Vou falar desse perigo na próxima parte.</p>
+
+        <p>Sabe aquele hábito saudável, como por exemplo "beber mais água", que você sempre quis ter mas nunca conseguiu desenvolver? Beber três litros de água, como recomendam por aí, parece muita coisa e difícil de cumprir, e três litros são seis (!!!) garrafinhas de água por dia! Difícil, né?</p>
+
+        <p>Mas e duas garrafas por bloco de dia? Duas de manhã, duas de tarde, duas de noite. Um pouco mais fácil assim, né?</p>
+
+        <p>E exercícios físicos? Lembra que você se propôs, em algum momento da sua vida de fazer caminhada todos os dias, ou ir na academia três vezes por semana? Talvez na resolução de ano novo? Mas o que aconteceu?</p>
+
+        <p>Para quem não está acostumado com exercícios físicos, uma simples caminhada pode demandar muita força de vontade. Mas e se for uma versão mais simples da tarefa? Como por exemplo, sair de casa, atravessar a rua e voltar? A única coisa que você precisa fazer, é colocar a roupa de caminhada, sair de casa, e voltar. Simples. Você pode tentar isso agora, se quiser.</p>
+
+        <Tip>Pegue a tarefa que você anotou no papel e tente decompô-la em três ou mais tarefas menores. Não se esqueça de anotar o resultado!</Tip>
+
+        <p>Criando o hábito de aplicar a decomposição em micro-tarefas, é possível que o diagrama de nossa vida se aproxime do seguinte:</p>
+
+        <Diagrama src="/static/texto-1-diagrama-tarefas-4.svg" />
+
+        <p>Agora vamos para o mais difícil, mas talvez o mais importante. Ainda não falei sobre a <strong>segunda</strong> coisa que precisamos saber para compreende melhor sobre <strong>progresso</strong>, mas vamos primeiro falar sobre como aproximar esse terceiro círculo dos outros dois.</p>
+
+        <h3>Como construir um caminho de vida</h3>
+
+        <p>Como já mencionei, movimentar o círculo de “coisas que sei porque devo fazer e que quero fazer” a nosso favor é extremamente difícil. Porém, apesar de difícil não quer dizer que não conseguiremos progredir nisso aos poucos. A boa notícia, é que assim como fizemos anteriormente, podemos construir micro-tarefas para, pouco a pouco, construir nosso caminho de vida.</p>
+
+        <p>Lembra de quando você era criança e estava aprendendo as operações básicas de matemática? Faz muito tempo, mas eu pelo menos tenho memórias vívidas de momentos que eu estava muito frustado de ter que quebrar a cabeça com “aquelas coisas inúteis”. Essas "coisas inúteis” era aprender a multiplicar e dividir.</p>
+
+        <p>Claro que é muito mais difícil mostrar para uma criança a importância do estudo (mais difícil mas não impossível). Mas não somos mais crianças, e continuamos sendo “obrigados” a aprender coisas na faculdade, a fazer coisas em nossos trabalhos, sem ter a plena consciência de porque essas coisas são importantes.</p>
+
+        <p>Depois que a fase criança passou, aprendemos a importância de saber multiplicar e dividir. Mas será que precisamos deixar passar a fase em que estamos agora para, quem sabe no futuro, descobrirmos porque devemos continuar fazendo o que fazemos hoje?</p>
+
+        <p>Não estou falando que as nossas obrigações não são importantes e que não acrescentam nada para nós ou para a sociedade. Estou apenas dizendo: porque essa importância não é tão clara para nós, no presente? E como podemos fazer para descobrí-las?</p>
+
+        <p>Como crianças não tivemos condições de descobrirmos, sozinhos, a importância da matemática básica. Saberemos descobrir por conta própria a importância de nossa vida atual, para nós mesmos?</p>
+
+        <p>Vamos aos poucos então, conquistando pequenos progressos.</p>
+
+        <Tip>1. Pegue a tarefa que você anotou no papel e pense: qual benefício terei, a curto ou médio prazo, ao concluir essa tarefa?</Tip>
+
+        <p>Percebe que não estamos falando mais de consequência negativa, mas sim de consequência positiva. Um pouco mais difícil, dependendo da tarefa, mas não tanto, certo? Agora vamos refletir com um pouco mais de profundidade.</p>
+
+        <Tip>2. Pegue a tarefa que você anotou no papel e pense: essa tarefa contribui de alguma forma, mesmo que pouco, para quem eu pretendo ser no futuro e/ou para o(s) caminho(s) que defini para mim? Se sim, anote o porquê.</Tip>
+
+        <p>Não gaste muito tempo nisso. O objetivo aqui não é definir pra sempre aquele propósito de nossas vidas que tanto temos procurado. É apenas progredir um pouco. Como se fosse uma micro-tarefa.</p>
+
+        <p>Se tivermos um pouquinho mais de clareza de porque devemos fazer, ou porque não devemos fazer nossas obrigações, ficaremos mais motivados em continuar, no caso de concluírmos que devemos continuar. Ou ficaremos mais motivados em mudar de direção, no caso de concluírmos que não devemos continuar.</p>
+
+        <p>Se você estiver completamente perdido, e não tiver nenhum caminho definido para sí próprio, nem souber quem você pretende ser, calma que é justamente sobre isso que falaremos a seguir.</p>
+
+        <Tip>3. Caso você tenha conseguido escrever o “porquê” no passo 2, reflita mais uma vez: Porque esse “porquê” é importante para você, ou para outras pessoas?</Tip>
+
+        <p>Sabe aquele criança chata, que talvez já fomos um dia, que não para de questionar o porquê das coisas até querer saber a origem do universo, e que os pais geralmente mandam ele parar de fazer pergunta besta ou dizem “porque eu disse e pronto!”?</p>
+
+        <p>Então, essa característica de questionar profundamente as coisas, por algum motivo nós perdemos. E seria interessante se, pouco a pouco, pudéssemos tentar aplicá-la no nosso dia a dia, para pouco a pouco chegarmos em um diagrama que gostaríamos de viver.</p>
+
+        <Diagrama src="/static/texto-1-diagrama-tarefas-5.svg" />
+
+        <p>Se você ficou perdido ou não conseguiu progredir em algum desses três últimos passos (ou para toda vez que você não consegue progredir em alguma tarefa), então precisamos do <strong>segundo</strong> entendimento sobre <strong>progresso</strong>: recontextualização.</p>
+
+        <h3>Como redefinir meu contexto atual de vida</h3>
+
+        <p>Sabe aquele raro momento em que estamos sendo super produtivos? Progredindo super rápido nas nossas tarefas? Até que chegamos naquela tarefa que agarra tudo. Ficamos horas ou até mesmo dia até conseguir passar dessa barreira que impede nosso progresso. Ficamos frustrados, ansiosos, </p>
+
+
+        <p>Recontextualização</p> <div>Quando você não souber o que fazer, a sua tarefa atual é descobrir o que fazer.</div> <div>Quando você não souber o seu objetivo de vida, seu objetivo atual é descobrir seu objetivo de vida.</div> <div>Quando você não souber o seu próximo passo, ou seu passo atual é descobrir o próximo passo.</div> <div>Quando você não souber para onde ir ou qual o seu caminho, seu objetivo é descobrir qual é o seu caminho.</div>
+    </LayoutPost>
+);
