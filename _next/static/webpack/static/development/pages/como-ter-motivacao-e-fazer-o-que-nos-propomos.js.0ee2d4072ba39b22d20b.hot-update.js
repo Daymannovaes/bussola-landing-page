a2eb5c97cdfed921a9bd1027ad2a1a5eed35c45c
Dayman Novaes <daymannovaes@gmail.com>
2018-11-23 22:02:00 -0200

    Updates

---
+++ _next/static/webpack/static/development/pages/como-ter-motivacao-e-fazer-o-que-nos-propomos.js.0ee2d4072ba39b22d20b.hot-update.js
@@ -0,0 +1,127 @@
+webpackHotUpdate("static/development/pages/como-ter-motivacao-e-fazer-o-que-nos-propomos.js",{
+
+/***/ "./pages/como-ter-motivacao-e-fazer-o-que-nos-propomos.js":
+/*!****************************************************************!*\
+  !*** ./pages/como-ter-motivacao-e-fazer-o-que-nos-propomos.js ***!
+  \****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_LayoutPost__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/LayoutPost */ "./components/LayoutPost.js");
+/* harmony import */ var _components_PostImg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/PostImg */ "./components/PostImg.js");
+/* harmony import */ var _components_MailChimp__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/MailChimp */ "./components/MailChimp.js");
+/* harmony import */ var _components_Tip__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../components/Tip */ "./components/Tip.js");
+/* harmony import */ var _components_texto_1_diagrama__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/texto-1-diagrama */ "./components/texto-1-diagrama.js");
+var _jsxFileName = "/Users/daymannovaes/workspace/bussola-landing-page/pages/como-ter-motivacao-e-fazer-o-que-nos-propomos.js";
+
+/* eslint-disable */
+
+
+
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_LayoutPost__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    title: "Como ter motiva\xE7\xE3o e sempre fazer o que nos propomos?",
+    date: 1539970091528,
+    id: "como-ter-motivacao-e-fazer-o-que-nos-propomos",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "Por que n\xE3o temos motiva\xE7\xE3o?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, "Poucas coisas na vida nos d\xE3o muita motiva\xE7\xE3o. E para piorar, raramente s\xE3o as mesmas coisas que temos obriga\xE7\xE3o de fazer."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, "Como mostra o diagrama abaixo, passamos a maior parte de nossas vidas, fazendo tarefas que n\xE3o queremos estar fazendo, ou que n\xE3o estamos plenamente felizes ao faz\xEA-las, o que n\xE3o contribuem de forma clara para o nosso futuro."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_texto_1_diagrama__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    src: "/static/texto-1-diagrama-tarefas-2.svg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, "Existem muitos fatores que contribuem para manter esses tr\xEAs c\xEDrculos separados e, com certeza, a falta de compreens\xE3o de do que \xE9 composto nossa ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, "motiva\xE7\xE3o"), " \xE9 um deles."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    },
+    __self: this
+  }, "Nesse texto completo, n\xF3s explicamos como conquistar aquilo voc\xEA procurava ao clicar nesse texto, como unir ao m\xE1ximo esses tr\xEAs c\xEDrculos de nossa vida."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_texto_1_diagrama__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    src: "/static/texto-1-diagrama-tarefas-6.svg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_MailChimp__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    text: "",
+    location: "texto-1-want-more",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }, "Espero que eu tenha te ajudado nesse breve per\xEDodo que passamos juntos, e espero tamb\xE9m que voc\xEA consiga aproximar seus tr\xEAs c\xEDrculos do diagrama. Qualquer coisa, deixa um coment\xE1rio a\xED em baixo e vamos conversando :)"));
+});
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/como-ter-motivacao-e-fazer-o-que-nos-propomos")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=como-ter-motivacao-e-fazer-o-que-nos-propomos.js.0ee2d4072ba39b22d20b.hot-update.js.map
