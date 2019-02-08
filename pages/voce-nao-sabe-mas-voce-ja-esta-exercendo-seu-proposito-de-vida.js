b9bd027adc1fe019804c3a12ae1c9d7482aecf5f
Dayman Novaes <daymannovaes@gmail.com>
2019-02-08 16:53:49 -0200

    text #4

---
+++ pages/voce-nao-sabe-mas-voce-ja-esta-exercendo-seu-proposito-de-vida.js
@@ -0,0 +1,206 @@
+/* eslint-disable */
+import React from 'react';
+
+import LayoutPost from '../components/LayoutPost';
+import PostImg from '../components/PostImg';
+import MailChimp from '../components/MailChimp';
+import Tip from '../components/Tip';
+import Link from '../components/Link';
+
+export default () => (
+    <LayoutPost title="Você não sabe, mas você já está exercendo seu propósito de vida" date={1549647726046} id="voce-nao-sabe-mas-voce-ja-esta-exercendo-seu-proposito-de-vida" image="texto-2-cover.jpg">
+        <p>Um propósito de vida é aquilo que ressoa com seu ser, que ressoa com aquilo que você valoriza na sua essência mais profunda.</p>
+
+        <p>Viver com propósito é buscar, <b>a todo momento</b>, expressar essa essência. Como se fosse uma cachoeira, que <b>a todo momento </b>flui, e é o próprio ato de fluir pela pedra que cria o caminho pelo qual a cachoeira pode seguir.</p>
+
+        <PostImg src="/static/texto-4-cachoeira-1.jpg" />
+
+        <p>Você diria que vive esse estilo de vida?</p>
+
+        <p>Eu diria que sim. Que você vive sim esse estilo de vida, e talvez nem saiba.</p>
+
+        <p>Se você acha que não, responda essa simples pergunta: quais são os seus valores mais profundos?</p>
+
+        <p>Não é tão simples, né?</p>
+
+        <p>Todo ser humano tem um conjunto muito particular de valores e habilidades.</p>
+
+        <p>TODO ser humano busca, inconscientemente, expressar esses valores na sua relação com o mundo.</p>
+
+        <p>Todo ser humano faz isso. E isso inclui você (provavelmente… cachorros não sabem usar a internet ainda… eu acho).</p>
+
+        <p>O que quero dizer por busca inconsciente?</p>
+
+        <p>O que quero dizer por expressar valores?</p>
+
+        <p>Calma, uma coisa de cada vez.</p>
+
+        <h3>Busca inconsciente</h3>
+
+        <p>A todo momento fazemos coisas sem perceber.</p>
+
+        <p>Por exemplo: no momento que você está lendo esse texto, você percebia que estava respirando?</p>
+
+        <Tip>Nesse momento sua respiração automática foi desativada. <small>... você precisa respirar</small></Tip>
+
+        <p>Desculpa. Foi só pra descontrair. Continuando...</p>
+
+        <p>Além de respirar, há várias coisas mais que fazemos sem perceber.</p>
+
+        <p>Sabe aquele seu amigo meio estressado, que com certa frequência responde aos outros de forma meio rude? Então, é bem provável que, em alguns momentos, essa pessoa nem perceba que faz isso.</p>
+
+        <p>Antes de julgar o próximo, pense: é bem capaz que você também faça isso, mesmo que só as vezes e sem perceber.
+        Se não faz, com certeza já fez alguma vez, quando estava cansado ou estressado com outra coisa.</p>
+
+        <p>Além dos hábitos bons e ruins que fazemos sem perceber (como respirar e ser rude), também buscamos valores específicos sem perceber.</p>
+
+
+    <h3>O que são valores?</h3>
+
+        <p>De forma bem resumida, são características que valorizamos, que buscamos, que desejamos atingir. Características com “valor intrínseco”.</p>
+
+        <p>Essa busca é tão intrínseca no ser humano, que é possível observá-la em várias culturas.</p>
+
+        <p>Os Samurais, os antigos guerreiros do império japonês, tinham um código de conduta, chamado de Bushidô (se traduz literalmente para: o caminho do guerreiro), que continha nele sete virtudes, que deveriam ser perseguidas a todo custo pelos guerreiros:</p>
+
+        <ul>
+            <li>
+                Justiça e integridade (義 gi)
+            </li>
+            <li>
+                Coragem (勇 yū)
+            </li>
+            <li>
+                Benevolência (仁 jin)
+            </li>
+            <li>
+                Respeito aos outros (礼 rei)
+            </li>
+            <li>
+                Sinceridade (誠 makoto)
+            </li>
+            <li>
+                Coerência nas ações (名誉 meiyo)
+            </li>
+            <li>
+                Responsabilidade (忠義 chūgi)
+            </li>
+        </ul>
+
+        <PostImg src="/static/texto-4-bushido.jpg" />
+
+        <p>Eu gosto de fazer o contraste com outra cultura para ficar ainda mais claro que são valores intrínsecos do ser humano, pois o mesmo código de conduta também existe nas religiões ocidentais.</p>
+
+        <p>Cada uma dessas sete virtudes são valorizadas por nós, e cada um de nós as busca em quantidades diferentes. Há pessoas que valorizam mais coragem do que benevolência, por exemplo.</p>
+
+        <p>Mas essas virtudes são apenas exemplos de valores, existem outros, sob vários nomes.</p>
+
+        <p>À medida que damos mais importância para um valor, buscamos expressá-lo cada vez mais nas nossas relações, mesmo que sem perceber - <b>o buscamos inconscientemente</b>.</p>
+
+        <p>Quais relações? Todas.</p>
+
+        <p>Na escolha da profissão, na decoração da nossa casa, nos nossos hobbies (filmes, livres, esportes…), nas nossas relações pessoais (amigos, familiares, desconhecidos…) e em qualquer contexto de vida.</p>
+
+        <p>Olhando para o seu passado e para seu presente com atenção e carinho, você vai descobrir quais são as coisas mais valorosas para você.</p>
+
+        <p>Por exemplo nos hobbies, nas séries e livros: quais são os personagens que mais te atraem? Você já percebeu que provavelmente existem características semelhantes entre eles?</p>
+
+        <p>Esses são os meus:</p>
+
+        <ul>
+            <li>
+                Sherlock Holmes
+            </li>
+            <li>
+                Kvothe, do O Nome do Vento
+            </li>
+            <li>
+                Sheldon, do The Big Bang Theory
+            </li>
+            <li>
+                Ross, de Friends
+            </li>
+        </ul>
+
+        <p>Eu só percebi isso muito tempo depois, mas todos têm algo em comum.</p>
+
+        <p>São todos personagens inteligentes, com um pensamento extremamente analítico e que, em algum grau, são incompreendidos na maioria das vezes.</p>
+
+        <PostImg src="/static/texto-4-sherlock.jpg" />
+
+        <p>Isso quer dizer que sou inteligente e analítico? Não. Apenas que são características que valorizo. E mesmo sem perceber, acabo buscando essas coisas ao me relacionar com o mundo.</p>
+
+        <Tip>Então tenho que descobrir meus personagens preferidos para descobrir meu propósito?</Tip>
+
+        <p>Claro que não. Quer dizer… sim, se você quiser, mas não necessariamente.</p>
+
+        <p>É só um exemplo de como nos identificamos com determinadas características, mais do que com outras, sem perceber. Esses foram personagens que sempre gostei muito, porém passei muito tempo sem perceber a semelhança entre eles.</p>
+
+        <p>Essa busca inconsciente por expressar nossos valores é, <b>por si só</b>, a expressão inconsciente de nosso propósito. É a expressão inconsciente daquilo que ressoa com a essência de nosso ser. E fazemos isso a todo momento.</p>
+
+        <p>O que podemos fazer então?</p>
+
+        <h3>Se tornando consciente dessa busca</h3>
+
+        <p>O que podemos e devemos fazer, é se tornar consciente dessa busca.</p>
+
+        <p>É estar presente, desperto e com a mente clara, e olhar para as situações de nossa vida e enxergar aquele padrão de valores que sempre buscamos, sem perceber.</p>
+
+        <p>É o eterno auto-conhecimento. O eterno observar-se. Tão eterno quanto o correr da água de uma cachoeira. E precisamos ter a coragem de fazer isso com sinceridade e honestidade.</p>
+
+        <p>Uma vez que fazemos isso, é possível de descobrirmos coisas que não gostamos tanto.</p>
+
+        <p>Por exemplo, ao olhar para esses quatro personagens, percebi que todos são, em algum grau, um pouco arrogantes.</p>
+
+        <p>Então olhei para mim mesmo, e percebi em mim o quanto eu fazia o mesmo em várias situações. Percebi então que, de alguma forma, eu estava sendo incoerente com aquilo que queria para mim.</p>
+
+        <p>Nesse momento podemos ir para o próximo passo: intensificar a busca.</p>
+
+        <p>Se eu percebo que a arrogância é algo que faz parte de mim, devo buscar conscientemente ações e situações que sejam capazes de expressar o contrário: respeito e humildade.</p>
+
+        <p>Como fazer isso? Bom… cada um encontra uma forma diferente de expressar essa busca, o importante mesmo, é estar consciente dessa busca, e buscar cada vez mais intensamente.</p>
+
+        <p>Então resumindo,</p>
+
+        <ol>
+            <li>
+                Descubra os seus valores mais essenciais
+            </li>
+            <ol>
+                <li>
+                    Olhe para dentro de si
+                </li>
+                <li>
+                    Olhe para seu passado, para os momentos mais marcantes e mais importantes que te moldaram
+                </li>
+                <li>
+                    Quais as coisas que você valoriza nas suas relações pessoais?
+                </li>
+                <li>
+                    Pergunte a seus amigos como eles te enxergam como pessoa
+                </li>
+            </ol>
+            <li>
+                Busque expressar esses valores cada vez mais em todos os contextos de vida
+            </li>
+            <li>
+                Se encontrar alguma incoerência entre valores, tenha coragem de abandoná-los ou transformá-los
+            </li>
+            <li>
+                Repita o processo, constantemente
+            </li>
+        </ol>
+
+        <p>Repita intensamente esse processo. Busque intensamente esse fluir dentro de você. Como uma Bússola guiando uma Cachoeira.</p>
+
+        <PostImg src="/static/texto-4-cachoeira-2.jpg" />
+
+        <p>Essa Bússola é a verdadeira Escola.</p>
+
+        <br />
+        <hr />
+
+        <p>A Escola da Bússola ajuda as pessoas a se encontrar no mundo e expressar seus valores cada vez mais.</p>
+        <p>Clique para saber mais.</p>
+    </LayoutPost>
+);
