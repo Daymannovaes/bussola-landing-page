323883ca778423b8b03cd2ccf7a308f281ba5c82
Dayman Novaes <daymannovaes@gmail.com>
2019-03-21 11:29:23 -0300

    Updates

---
+++ _next/static/webpack/static/development/pages/como-uma-referencia-temporal-pode-mudar-sua-vida.js.b63eee3295d413c82ca9.hot-update.js
@@ -0,0 +1,389 @@
+webpackHotUpdate("static/development/pages/como-uma-referencia-temporal-pode-mudar-sua-vida.js",{
+
+/***/ "./pages/como-uma-referencia-temporal-pode-mudar-sua-vida.js":
+/*!*******************************************************************!*\
+  !*** ./pages/como-uma-referencia-temporal-pode-mudar-sua-vida.js ***!
+  \*******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_LayoutPost__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/LayoutPost */ "./components/LayoutPost.js");
+/* harmony import */ var _components_PostImg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/PostImg */ "./components/PostImg.js");
+/* harmony import */ var _components_PostImgColored__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/PostImgColored */ "./components/PostImgColored.js");
+/* harmony import */ var _components_MailChimp__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../components/MailChimp */ "./components/MailChimp.js");
+/* harmony import */ var _components_Tip__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/Tip */ "./components/Tip.js");
+/* harmony import */ var _components_Link__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../components/Link */ "./components/Link.js");
+var _jsxFileName = "/Users/daymannovaes/workspace/bussola-landing-page/pages/como-uma-referencia-temporal-pode-mudar-sua-vida.js";
+
+/* eslint-disable */
+
+
+
+
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_LayoutPost__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    title: "Como uma refer\xEAncia temporal pode mudar a sua vida",
+    date: 1544099560766,
+    id: "como-uma-referencia-temporal-pode-mudar-sua-vida",
+    image: "texto-3-estrelas.jpg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, "Meio sensacionalista o t\xEDtulo n\xE9?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, "A outra alternativa era \"como fazer as coisas certas pode te levar at\xE9 \xE0 lua\", mas achei que ficaria ainda mais sensacionalista."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "Calma, eu j\xE1 explico o que uma refer\xEAncia temporal tem a ver com a lua e o que isso tem a ver com a sua vida."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, "Mapa espacial"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, "Todos n\xF3s sabemos que para navegar bem em um territ\xF3rio, precisamos conhecer bem o nosso entorno. Seja em uma floresta, seja dentro do shopping ou seja viajando, todos n\xF3s usamos uma esp\xE9cie de ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, "refer\xEAncia espacial"), ", que chamamos de ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, "mapa"), "."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23
+    },
+    __self: this
+  }, "As vezes esse mapa est\xE1 apenas dentro de nossas cabe\xE7as (voc\xEA sabe onde ficam os c\xF4modos da sua casa, certo?), as vezes em papel ou as vezes no celular, com o aux\xEDlio do GPS."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_PostImgColored__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    bgColor: [241, 185, 95],
+    src: "/static/texto-3-mapa.jpg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, "Voc\xEA t\xE1 explicando coisa \xF3bvia, foi pra isso que cliquei no seu t\xEDtulo sensacionalista?"), " - Talvez voc\xEAs me perguntem, um pouco indignados. Calma! Essa explica\xE7\xE3o \xE9 apenas para entendermos algo mais complexo, em termos de coisas que todos j\xE1 conhecemos bem. Uma met\xE1fora."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, "Todo n\xF3s entendemos, com relativa facilidade, como um mapa funciona, porque todos n\xF3s temos ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, "no\xE7\xE3o espacial"), ". Por\xE9m, o que temos uma certa dificuldade em conceber \xE9 uma ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, "no\xE7\xE3o temporal"), "."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }, "Mapa temporal"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, "Me responde uma pergunta: quando voc\xEA olha um mapa espacial, voc\xEA sabe exatamente para onde est\xE1 indo, certo?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }, "Mas quando voc\xEA olha para a sua vida como um todo, voc\xEA consegue me dizer para onde est\xE1 indo? Qual dire\xE7\xE3o sua vida est\xE1 tomando? Voc\xEA sabe responder essa pergunta com clareza? Qual o seu prop\xF3sito? Voc\xEA tem um prop\xF3sito?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37
+    },
+    __self: this
+  }, "Quando olhamos para a nossa vida e falamos em prop\xF3sito, mudamos de uma dimens\xE3o espacial para uma dimens\xE3o temporal, que \xE9 o tempo."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39
+    },
+    __self: this
+  }, "A dimens\xE3o \xE9 outra, mas a l\xF3gica \xE9 a mesma: precisamos conhecer o nosso redor para saber navegar bem, precisamos de um ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39
+    },
+    __self: this
+  }, "mapa temporal"), "."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_PostImgColored__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    bgColor: [26, 24, 30],
+    textColor: [225, 225, 225],
+    src: "/static/texto-3-estrelas.jpg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 41
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "No espa\xE7o estamos ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "aqui"), ". No tempo estamos no ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "agora"), ". O ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }, "entorno"), " do presente \xE9 o passado e o futuro."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Tip__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 45
+    },
+    __self: this
+  }, "Um prop\xF3sito \xE9 uma refer\xEAncia de ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 45
+    },
+    __self: this
+  }, "futuro"), ", constru\xEDda como um reflexo de nossa identidade (de nosso ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 45
+    },
+    __self: this
+  }, "passado"), "), que nos guia no ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 45
+    },
+    __self: this
+  }, "presente"), "."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 47
+    },
+    __self: this
+  }, "Se mesmo tendo no\xE7\xE3o espacial, precisamos de mapa e GPS para nos guiar no espa\xE7o, porque n\xE3o nos preocupamos em construir um mapa temporal para nos guiar no tempo?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 49
+    },
+    __self: this
+  }, "Ter uma clara refer\xEAncia de futuro n\xE3o \xE9 simplesmente planejar sua pr\xF3xima refei\xE7\xE3o ou planejar o que voc\xEA vai fazer no in\xEDcio de um novo ano. Um ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 49
+    },
+    __self: this
+  }, "prop\xF3sito claro"), " \xE9 extremamente poderoso para guiarmos a nossa vida como n\xF3s mesmos quisermos."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 51
+    },
+    __self: this
+  }, "Nos d\xE1 mais liberdade, mais autonomia, mais felicidade. Kant dizia que n\xF3s exercemos nosso prop\xF3sito de vida \xE0 medida que expressamos nossa autonomia, e vice versa. Porque n\xE3o buscar isso?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 53
+    },
+    __self: this
+  }, "O que a lua tem a ver com isso?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_PostImg__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    src: "/static/texto-3-lua.jpg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 55
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 57
+    },
+    __self: this
+  }, "O livro The ONE Thing nos conta que, No Dia do Domin\xF3, em 13 de novembro de 2009, a Weijers Domino Productions quebrou o recorde mundial de queda de domin\xF3, alinhando mais de quatro milh\xF5es de pe\xE7as. Nesse evento, uma \xFAnica pe\xE7a colocou em movimento uma queda que acumulou 94.000 joules de energia."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 59
+    },
+    __self: this
+  }, "Cada domin\xF3 em p\xE9 representa uma pequena por\xE7\xE3o de energia potencial. Quanto mais s\xE3o alinhados, mais energia ser\xE1 liberada \xE0 partir de um \xFAnico movimento."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 61
+    },
+    __self: this
+  }, "Al\xE9m disso, um domin\xF3 (assim como qualquer outro objeto) \xE9 capaz de derrubar um objeto at\xE9 50% maior, acumulando ainda mais energia potencial. Alinhando domin\xF3s cada vez maiores, aumentamos exponencialmente o potencial do sistema."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63
+    },
+    __self: this
+  }, "Come\xE7ando com um pequeno domin\xF3 de 5 cent\xEDmetros, l\xE1 pelo 18\xBA, voc\xEA veria uma pe\xE7a quase do tamanho da Torre de Pisa. O 23\xBA domin\xF3 seria bem maior que a Torre Eiffel, e o 31\xBA, quase mil metros mais alto que o Monte Everest. ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("strong", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63
+    },
+    __self: this
+  }, "O 57\xBA cobriria, praticamente, a dist\xE2ncia entre a Terra e a Lua!")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_PostImg__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    src: "/static/texto-3-dominos.jpg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 65
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 67
+    },
+    __self: this
+  }, "Voc\xEA entende que quando alinhamos as coisas corretas, cada movimento acaba ajudando a produzir o pr\xF3ximo, produzindo resultados extraordin\xE1rios?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 69
+    },
+    __self: this
+  }, "O exemplo dos domin\xF3s nos conta como isso pode ser feito no espa\xE7o, por\xE9m, assim como o mapa, a l\xF3gica no tempo \xE9 a mesma: alinhe corretamente os eventos temporais da sua vida, e um ap\xF3s o outro, voc\xEA conquistar\xE1 coisas cada vez mais extraordin\xE1rias."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 71
+    },
+    __self: this
+  }, "Sabe aquelas palestras motivacionais que o palestrante fala \"voc\xEA \xE9 capaz!\"? Ent\xE3o, se voc\xEA souber para onde est\xE1 indo, e seguir o caminho que leva at\xE9 esse caminho, sim, voc\xEA \xE9 capaz de conquistar aquilo que voc\xEA j\xE1 se convenceu que n\xE3o seria capaz."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 73
+    },
+    __self: this
+  }, "Como construir um mapa temporal?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 75
+    },
+    __self: this
+  }, "N\xF3s n\xE3o enxergamos o tempo, n\xE3o conseguimos tocar nem sentir o tempo, n\xE3o conhecemos o futuro e o passado talvez nem exista. Tudo que conhecemos \xE9 o presente. Como ent\xE3o, definir um mapa nessa dimens\xE3o t\xE3o complicada?"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 77
+    },
+    __self: this
+  }, "Essa tarefa complicada \xE9 que estamos tentando resolver. Independente de qual seja a solu\xE7\xE3o, qual seja o caminho para encontrar a resposta, n\xF3s acreditamos que essa \xE9 uma das perguntas mais fundamentais que podemos te ajudar a responder."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 79
+    },
+    __self: this
+  }, "Enquanto n\xE3o existir uma \"b\xFAssola temporal\" para apontar o norte de sua vida, voc\xEA pode fornecer seu email para a bussola.school e teremos o maior prazer de encontrar essa resposta junto com voc\xEA."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_MailChimp__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    text: "",
+    buttonText: "quero ajuda!",
+    placeholder: "email da sua vida",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 81
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_PostImg__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    src: "/static/texto-3-bussola.jpg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 83
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 85
+    },
+    __self: this
+  }, "\xC9 dessa forma que uma refer\xEAncia temporal pode mudar a sua vida, \xE9 dessa forma que fazendo as coisas certas voc\xEA pode chegar at\xE9 \xE0 lua."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 87
+    },
+    __self: this
+  }, "O que voc\xEA acha disso? Comenta a\xED em baixo a sua opini\xE3o :)"));
+});
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/como-uma-referencia-temporal-pode-mudar-sua-vida")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=como-uma-referencia-temporal-pode-mudar-sua-vida.js.b63eee3295d413c82ca9.hot-update.js.map
